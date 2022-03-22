ff5dbf9a9d4c8fa82fe31309915385be40967ffb
esantosjunior <99204359+esantosjunior@users.noreply.github.com>
2022-03-22 00:52:45 -0300

    App Engine Provision

--- providers.tf
+++ providers.tf
@@ -10,8 +10,13 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-
 project = "labdevopscloud-enilsonjr"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
+
+provider "google-beta" {
+  project = "labdevopscloud-enilsonjr"
   region  = "us-west1"
   zone    = "us-west1-b"
 }
